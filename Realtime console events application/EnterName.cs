9b10c48923217cb1572880994849be3ee15e3889
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-15 17:59:01 +0530

    Add files via upload

---
+++ Realtime console events application/EnterName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+ public   class EnterName
+    {
+        public event EventHandler<BannedUserEventArgs> ev_BannedUser;
+        public void User()
+        {
+            Console.Write("Enter You Name : ");
+            string user = Console.ReadLine();
+
+            if ((user == "Jack" || user == "Steven" || user == "Mathew") && (ev_BannedUser != null))
+            {
+                ev_BannedUser(this, new BannedUserEventArgs(user));
+            }
+            else
+            {
+                Console.WriteLine("Welcome to " + user);
+            }
+        }
+    }
+}
---
+++ Realtime console events application/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello World!");
+            EnterName ename = new EnterName();
+            ename.ev_BannedUser += WarningAlarm;
+            ename.User();
+            Console.Read();
+        }
+        static void WarningAlarm(object sender, BannedUserEventArgs e)
+        {
+            Console.WriteLine("{0} Users Found. Sending Email to Administration.", e.Name);
+            Console.WriteLine("Email Sent.");
+            Console.WriteLine("Warning Alarm Started.");
+            Console.WriteLine("Press Ctrl + c to stop the alarm");
+            for (; ; )
+            {
+                Console.Beep();
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+
+}
+

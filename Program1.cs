9fc0a45c60eda88a6bbc2f17566d62346a14c20c
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-15 18:14:11 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates_and_events
+{
+ public   class Program1
+    {
+        static void Main()
+        {
+            Employee emp1 = new Employee()
+            {
+                ID = 101,
+                Name = "Pranaya",
+                Gender = "Male",
+                Experience = 5,
+                Salary = 10000
+            };
+            Employee emp2 = new Employee()
+            {
+                ID = 102,
+                Name = "Priyanka",
+                Gender = "Female",
+                Experience = 10,
+                Salary = 20000
+            };
+            Employee emp3 = new Employee()
+            {
+                ID = 103,
+                Name = "Anurag",
+                Experience = 15,
+                Salary = 30000
+            };
+            List<Employee> lstEmployess = new List<Employee>();
+            lstEmployess.Add(emp1);
+            lstEmployess.Add(emp2);
+            lstEmployess.Add(emp3);
+            Employee.PromoteEmployee(lstEmployess, x => x.Experience > 5);
+            Console.ReadKey();
+        }
+    }
+}

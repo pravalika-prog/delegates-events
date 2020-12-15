e0c2a7a56a0aa105a4b7f7c2e4ae1a111ae73dd9
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-15 18:24:13 +0530

    Add files via upload

---
+++ Events2/AddSum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+  public  class AddSum
+    {
+        public event EventHandler<OddNumberEventArgs> od_number;
+        public void fun()
+        {
+            int result;
+            result = 5 + 4;
+            Console.WriteLine(result.ToString());
+            if (result % 2 != 0)&& (od_number != null){
+                od_number(this, new OddNumberEventArgs(result));
+            }
+        }
+    }
+}
---
+++ Events2/program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+  public  class program1
+    {
+        static void Main()
+        {
+            AddSum s = new AddSum();
+            od_number += message;
+            s.fun();
+        }
+        staic void message( object sender, OddNumberEventArgs e)
+        {
+            Console.WriteLine("Event Executed : {0} is an Odd Number", e.sum);
+        }
+    }
+}

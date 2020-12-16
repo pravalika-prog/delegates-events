186bc7cb713794602bcb3d9faa0abac2547f2fb8
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-16 20:25:53 +0530

    Add files via upload

---
+++ event1/Eligible to apply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events2
+{
+  public  class Eligible_to_apply
+    {
+        public event EventHandler<personEventArgs> apply_person;
+       public void eligibility()
+        {
+            Console.WriteLine("enter the age ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the experience ");
+            int experience = int.Parse(Console.ReadLine());
+        if(age>30 && experience > 2)
+            {
+                Console.WriteLine("eligible to aply");
+                apply_person(this, new personEventArgs(age,experience));
+           
+            }
+            else
+            {
+                Console.WriteLine("not eligible to applly");
+            }
+        }
+    }
+}
---
+++ event1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Events2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello World!");
+            
+
+
+
+            Eligible_to_apply a = new Eligible_to_apply();
+            a.apply_person += showmessage;
+            a.eligibility();
+        }
+        static void showmessage(object sender, personEventArgs e)
+        {
+            Console.WriteLine("if your age is below 30 and experience is less than  2 :{0},{1}");
+            }
+        }
+    }
+
